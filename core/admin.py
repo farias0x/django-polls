6feb0106c9977ac0256d94e59b6beb6385fc4ca1
farias0x <luizclaudiofarias21@gmail.com>
2026-02-28 17:11:17 -0300

    Register question and choice models into admin site

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+from .models import Question, Choice
+
+AdminSite.site_header = "Django Polls"
+
+admin.site.register(Question)
+admin.site.register(Choice)
