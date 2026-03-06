3d1e934465fef0d4123d56074b1627f63534f03d
farias0x <luizclaudiofarias21@gmail.com>
2026-03-05 22:01:03 -0300

    Create tests for DetailView

--- core/tests.py
+++ core/tests.py
@@ -115,3 +115,17 @@ class QuestionIndexViewTests(TestCase):
             response.context["question_list"],
             [question2, question1],
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=4)
+        url = reverse("core:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse("core:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
